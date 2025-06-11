2eb3a180a02335f5d178520550dbde7a6f61bac4
Vilosofy <owenfarida14@gmail.com>
2025-06-11 20:18:43 +0700

    Pagination margin fix

--- src/components/roadmap/community-roadmap-list.tsx
+++ src/components/roadmap/community-roadmap-list.tsx
@@ -81,89 +81,89 @@ const CommunityRoadmap = ({
 
   return (
     <>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         {isPending ? (
-          <div className='col-span-full flex justify-center'>
-            <span className='loader'></span>
+          <div className="col-span-full flex justify-center">
+            <span className="loader"></span>
           </div>
         ) : roadmaps.length > 0 ? (
           roadmaps.map((roadmap) => (
             <RoadmapCard key={roadmap.id} roadmap={roadmap} />
           ))
         ) : (
-          <p className='text-center col-span-full text-gray-500'>
+          <p className="text-center col-span-full text-gray-500">
             No Data Available
           </p>
         )}
-      </div>
+        
+        {/* Pagination Controls */}
+        {showPagination && (
+          <div className="flex flex-wrap justify-center md:justify-between items-center text-body-1-regular">
+            {/* Showing page info */}
+            <div className="text-center mt-4">
+              <span>
+                Showing {1 + (currentPage - 1) * pageSize} to{" "}
+                {Math.min(currentPage * pageSize, totalData)} of {totalData}{" "}
+                results
+              </span>
+            </div>
 
-      {/* Pagination Controls */}
-      {showPagination && (
-        <div className='flex flex-wrap justify-center md:justify-between items-center text-body-1-regular'>
-          {/* Showing page info */}
-          <div className='text-center mt-4'>
-            <span>
-              Showing {1 + (currentPage - 1) * pageSize} to{" "}
-              {Math.min(currentPage * pageSize, totalData)} of {totalData}{" "}
-              results
-            </span>
-          </div>
-
-          <div className='flex justify-center items-center mt-6 space-x-0 mx-6'>
-            <button
-              className='py-2 px-4 bg-white-500 border border-black-100 rounded-l-lg hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50'
-              onClick={() => handlePageChange(1)}
-              disabled={currentPage === 1}
-            >
-              ⏪
-            </button>
-            <button
-              className='py-2 px-4 bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50'
-              onClick={() => handlePageChange(currentPage - 1)}
-              disabled={currentPage === 1}
-            >
-              ◀️
-            </button>
-
-            {/* Page Numbers with Ellipsis */}
-            {generatePageNumbers().map((page, index) => (
+            <div className="flex justify-center items-center mt-6 space-x-0">
               <button
-                key={index}
-                className={`py-2 px-4 ${
-                  page === currentPage
-                    ? "bg-blue-500 text-white-500 border-none"
-                    : page === "..."
-                    ? "bg-white-500 border border-black-100"
-                    : "bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0"
-                }`}
-                onClick={() =>
-                  page === "..."
-                    ? handleCustomPage()
-                    : handlePageChange(page as number)
-                }
-                disabled={page === "..."}
+                className="py-2 px-4 bg-white-500 border border-black-100 rounded-l-lg hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50"
+                onClick={() => handlePageChange(1)}
+                disabled={currentPage === 1}
               >
-                {page}
+                ⏪
               </button>
-            ))}
+              <button
+                className="py-2 px-4 bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50"
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                ◀️
+              </button>
 
-            <button
-              className='py-2 px-4 bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50'
-              onClick={() => handlePageChange(currentPage + 1)}
-              disabled={currentPage === totalPages}
-            >
-              ▶️
-            </button>
-            <button
-              className='py-2 px-4 bg-white-500 border border-black-100 rounded-r-lg hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50'
-              onClick={() => handlePageChange(totalPages)}
-              disabled={currentPage === totalPages}
-            >
-              ⏩
-            </button>
+              {/* Page Numbers with Ellipsis */}
+              {generatePageNumbers().map((page, index) => (
+                <button
+                  key={index}
+                  className={`py-2 px-4 ${
+                    page === currentPage
+                      ? "bg-blue-500 text-white-500 border-none"
+                      : page === "..."
+                      ? "bg-white-500 border border-black-100"
+                      : "bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0"
+                  }`}
+                  onClick={() =>
+                    page === "..."
+                      ? handleCustomPage()
+                      : handlePageChange(page as number)
+                  }
+                  disabled={page === "..."}
+                >
+                  {page}
+                </button>
+              ))}
+
+              <button
+                className="py-2 px-4 bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50"
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
+              >
+                ▶️
+              </button>
+              <button
+                className="py-2 px-4 bg-white-500 border border-black-100 rounded-r-lg hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50"
+                onClick={() => handlePageChange(totalPages)}
+                disabled={currentPage === totalPages}
+              >
+                ⏩
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 };
