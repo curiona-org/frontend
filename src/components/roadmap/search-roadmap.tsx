47a4ef4ab7ecbcd7c52bdb742876fdb3239a18a5
Vilosofy <owenfarida14@gmail.com>
2025-06-13 01:09:49 +0700

    Community Roadmap header font size fix

--- src/components/roadmap/search-roadmap.tsx
+++ src/components/roadmap/search-roadmap.tsx
@@ -19,38 +19,38 @@ const SearchBar = () => {
   }, [query]);
 
   return (
-    <div className='flex flex-col gap-4'>
-      <div className='flex flex-wrap gap-4 items-center font-satoshi text-display-1'>
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-wrap gap-4 items-center text-mobile-display-1 lg:text-display-1">
         <h1>Explore Community</h1>
-        <span className='text-blue-500 dashedBorder px-5 py-1'>Roadmaps</span>
+        <span className="text-blue-500 dashedBorder px-5 py-1">Roadmaps</span>
       </div>
-      <p className='font-satoshi text-heading-4-regular'>
+      <p className="font-satoshi text-heading-4-regular">
         Find inspiration in community-made roadmaps and start your own journey.
       </p>
-      <div className='flex items-center border border-gray-300 bg-white-500 rounded-lg overflow-hidden w-full h-14'>
+      <div className="flex items-center border border-gray-300 bg-white-500 rounded-lg overflow-hidden w-full h-14">
         {/* Input Search */}
-        <Image src={Logo} alt='Logo' className='relative left-3' />
+        <Image src={Logo} alt="Logo" className="relative left-3" />
         <input
-          type='text'
-          placeholder='Type a keyword to explore roadmaps...'
+          type="text"
+          placeholder="Type a keyword to explore roadmaps..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          className='w-full px-6 py-2 text-black-500 focus:outline-none'
+          className="w-full px-6 py-2 text-black-500 focus:outline-none"
         />
 
         {/* Dropdown Filter */}
-        <div className='relative'>
+        <div className="relative">
           <select
-            className='px-4 py-2 bg-white text-black border-l border-gray-300 cursor-pointer focus:outline-none'
+            className="px-4 py-2 bg-white text-black border-l border-gray-300 cursor-pointer focus:outline-none"
             value={orderBy}
             onChange={(e) => setOrderBy(e.target.value as "newest" | "oldest")}
           >
-            <option value='newest'>Newest</option>
-            <option value='oldest'>Oldest</option>
+            <option value="newest">Newest</option>
+            <option value="oldest">Oldest</option>
           </select>
         </div>
       </div>
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <CommunityRoadmapList search={debouncedQuery} orderBy={orderBy} />
       </div>
     </div>
