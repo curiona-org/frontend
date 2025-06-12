00354c8ef544d285adf67c841ef9ea972497b3e3
HotPotatoC <juandotulung@gmail.com>
2025-06-12 18:25:56 +0700

    hotfix: race-condition in roadmap detail

--- src/app/roadmap/[slug]/page.tsx
+++ src/app/roadmap/[slug]/page.tsx
@@ -1,6 +1,10 @@
-import { getRoadmapBySlug } from "@/app/roadmap/[slug]/actions";
+"use client";
+import Loader from "@/components/loader/loader";
 import RoadmapDetailClient from "@/components/roadmap/roadmap-detail-client";
+import { RoadmapService } from "@/lib/services/roadmap.service";
+import { GetRoadmapOutput } from "@/types/api-roadmap";
 import { notFound } from "next/navigation";
+import { use, useEffect, useState, useTransition } from "react";
 
 interface RoadmapDetailPageProps {
   params: Promise<{
@@ -8,17 +12,33 @@ interface RoadmapDetailPageProps {
   }>;
 }
 
-export default async function RoadmapDetailPage({
-  params,
-}: RoadmapDetailPageProps) {
-  const { slug } = await params;
-  const result = await getRoadmapBySlug(slug);
+const roadmapService = new RoadmapService();
+export default function RoadmapDetailPage({ params }: RoadmapDetailPageProps) {
+  const { slug } = use(params);
+  const [roadmap, setRoadmap] = useState<GetRoadmapOutput | null>(null);
+  const [isPending, startTransition] = useTransition();
 
-  if (!result.success || !result.data) {
-    notFound();
+  useEffect(() => {
+    async function fetchRoadmap() {
+      const result = await roadmapService.getRoadmapBySlug(slug);
+      if (!result?.data) {
+        notFound();
+      } else {
+        startTransition(() => {
+          setRoadmap(result.data);
+        });
+      }
+    }
+    fetchRoadmap();
+  }, [slug]);
+
+  if (isPending || !roadmap) {
+    return (
+      <div className='fixed inset-0 z-[999] bg-white-500 flex items-center justify-center'>
+        <Loader />
+      </div>
+    );
   }
-
-  const roadmap = result.data;
 
   return <RoadmapDetailClient initialRoadmap={roadmap} slug={slug} />;
 }
