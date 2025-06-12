50d95d10d191720e3a6fb97579c43386057e042e
Vilosofy <owenfarida14@gmail.com>
2025-06-12 21:54:57 +0700

    Community Roadmap Page Pagination Fix

--- src/components/roadmap/community-roadmap-list.tsx
+++ src/components/roadmap/community-roadmap-list.tsx
@@ -108,17 +108,17 @@ const CommunityRoadmap = ({
 
   return (
     <>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
         {isPending ? (
-          <div className='col-span-full flex justify-center'>
-            <span className='loader'></span>
+          <div className="col-span-full flex justify-center">
+            <span className="loader"></span>
           </div>
         ) : roadmaps.length > 0 ? (
           roadmaps.map((roadmap) => (
             <RoadmapCard key={roadmap.id} roadmap={roadmap} />
           ))
         ) : (
-          <p className='text-center col-span-full text-gray-500'>
+          <p className="text-center col-span-full text-gray-500">
             No Data Available
           </p>
         )}
@@ -126,9 +126,9 @@ const CommunityRoadmap = ({
 
       {/* Pagination Controls */}
       {showPagination && (
-        <div className='flex flex-wrap justify-center md:justify-between items-center text-mobile-body-1-regular lg:text-body-1-regular'>
+        <div className="flex flex-wrap justify-center md:justify-between items-center text-mobile-body-1-regular lg:text-body-1-regular">
           {/* Showing page info */}
-          <div className='text-center mt-4'>
+          <div className="text-center mt-4">
             <span>
               Showing {1 + (currentPage - 1) * pageSize} to{" "}
               {Math.min(currentPage * pageSize, totalData)} of {totalData}{" "}
@@ -136,16 +136,16 @@ const CommunityRoadmap = ({
             </span>
           </div>
 
-          <div className='flex justify-center items-center mt-6 space-x-0'>
+          <div className="flex justify-center items-center mt-6 space-x-0">
             <button
-              className='py-2 px-4 bg-white-500 border border-black-100 rounded-l-lg hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50'
+              className="w-8 h-8 bg-white-500 border border-black-100 rounded-l-lg hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50"
               onClick={() => handlePageChange(1)}
               disabled={currentPage === 1}
             >
               ⏪
             </button>
             <button
-              className='py-2 px-4 bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50'
+              className="w-8 h-8 bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50"
               onClick={() => handlePageChange(currentPage - 1)}
               disabled={currentPage === 1}
             >
@@ -175,14 +175,14 @@ const CommunityRoadmap = ({
             ))}
 
             <button
-              className='py-2 px-4 bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50'
+              className="w-8 h-8 bg-white-500 border border-black-100 hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50"
               onClick={() => handlePageChange(currentPage + 1)}
               disabled={currentPage === totalPages}
             >
               ▶️
             </button>
             <button
-              className='py-2 px-4 bg-white-500 border border-black-100 rounded-r-lg hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50'
+              className="w-8 h-8 bg-white-500 border border-black-100 rounded-r-lg hover:bg-gray-200 focus:outline-none focus:ring-0 disabled:opacity-50"
               onClick={() => handlePageChange(totalPages)}
               disabled={currentPage === totalPages}
             >
