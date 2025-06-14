68197034a0a0a32a5dead7067fb8e4deb8e2dd03
HotPotatoC <juandotulung@gmail.com>
2025-06-14 17:56:10 +0700

    feat: removed border in user avatar

--- src/components/profile/personal-information.tsx
+++ src/components/profile/personal-information.tsx
@@ -76,40 +76,40 @@ const PersonalInformation = ({ data }: PersonalInformationProps) => {
   };
 
   return (
-    <div className="flex flex-wrap md:flex-nowrap gap-4 md:gap-8 items-center justify-between">
+    <div className='flex flex-wrap md:flex-nowrap gap-4 md:gap-8 items-center justify-between'>
       <img
         src={data.avatar}
         alt={data.name}
-        className="size-28 select-none rounded-full object-cover border-4 border-blue-500"
+        className='size-28 select-none rounded-full object-cover'
       />
-      <div className="w-full flex flex-col gap-4">
-        <p className="text-mobile-heading-4-bold lg:text-heading-4-bold font-semibold text-gray-800">
+      <div className='w-full flex flex-col gap-4'>
+        <p className='text-mobile-heading-4-bold lg:text-heading-4-bold font-semibold text-gray-800'>
           Personal Information
         </p>
-        <div className="flex flex-col gap-2">
-          <div className="md:grid md:grid-cols-3 md:gap-x-12 overflow-hidden">
-            <div className="w-full flex justify-between items-center md:block px-1">
-              <p className="flex text-mobile-body-1-medium lg:text-body-1-medium text-black-200 items-center mb-2">
+        <div className='flex flex-col gap-2'>
+          <div className='md:grid md:grid-cols-3 md:gap-x-12 overflow-hidden'>
+            <div className='w-full flex justify-between items-center md:block px-1'>
+              <p className='flex text-mobile-body-1-medium lg:text-body-1-medium text-black-200 items-center mb-2'>
                 <span
-                  role="img"
-                  aria-label="user"
-                  className="mr-2 flex-shrink-0"
+                  role='img'
+                  aria-label='user'
+                  className='mr-2 flex-shrink-0'
                 >
                   🙂
                 </span>
                 Full Name
               </p>
               <p
-                className="text-mobile-body-1-regular lg:text-body-1-regular break-words"
+                className='text-mobile-body-1-regular lg:text-body-1-regular break-words'
                 title={editing ? undefined : newName}
               >
                 {editing ? (
                   <>
                     <input
-                      type="text"
+                      type='text'
                       value={newName}
                       onChange={handleNameChange}
-                      className="w-full border-b-2 border-blue-600 focus:outline-none bg-blue-50"
+                      className='w-full border-b-2 border-blue-600 focus:outline-none bg-blue-50'
                       autoFocus
                     />
                   </>
@@ -118,37 +118,37 @@ const PersonalInformation = ({ data }: PersonalInformationProps) => {
                 )}
               </p>
             </div>
-            <div className="w-full flex justify-between items-center md:block px-1">
-              <p className="flex text-mobile-body-1-medium lg:text-body-1-medium text-black-200 items-center mb-2">
+            <div className='w-full flex justify-between items-center md:block px-1'>
+              <p className='flex text-mobile-body-1-medium lg:text-body-1-medium text-black-200 items-center mb-2'>
                 <span
-                  role="img"
-                  aria-label="email"
-                  className="mr-2 flex-shrink-0"
+                  role='img'
+                  aria-label='email'
+                  className='mr-2 flex-shrink-0'
                 >
                   📬
                 </span>
                 Email
               </p>
               <p
-                className="text-mobile-body-1-regular lg:text-body-1-regular break-words"
+                className='text-mobile-body-1-regular lg:text-body-1-regular break-words'
                 title={data.email}
               >
                 {data.email}
               </p>
             </div>
-            <div className="w-full flex justify-between items-center md:block px-1">
-              <p className="flex text-mobile-body-1-medium lg:text-body-1-medium text-black-200 items-center mb-2">
+            <div className='w-full flex justify-between items-center md:block px-1'>
+              <p className='flex text-mobile-body-1-medium lg:text-body-1-medium text-black-200 items-center mb-2'>
                 <span
-                  role="img"
-                  aria-label="calendar"
-                  className="mr-2 flex-shrink-0"
+                  role='img'
+                  aria-label='calendar'
+                  className='mr-2 flex-shrink-0'
                 >
                   🏠
                 </span>
                 Joined On
               </p>
               <p
-                className="text-mobile-body-1-regular lg:text-body-1-regular break-words"
+                className='text-mobile-body-1-regular lg:text-body-1-regular break-words'
                 title={new Date(data.joined_at).toLocaleDateString("en-GB", {
                   day: "2-digit",
                   month: "long",
@@ -164,38 +164,38 @@ const PersonalInformation = ({ data }: PersonalInformationProps) => {
             </div>
           </div>
           {editNameError && (
-            <p className="text-red-500 text-mobile-body-1-regular lg:text-body-1-regular">
+            <p className='text-red-500 text-mobile-body-1-regular lg:text-body-1-regular'>
               {editNameError}
             </p>
           )}
         </div>
       </div>
       {/* Buttons */}
-      <div className="w-full md:w-fit flex gap-4">
+      <div className='w-full md:w-fit flex gap-4'>
         {editing ? (
           <>
             <Button
               onClick={handleSaveClick}
               disabled={editNameError !== ""}
-              className="flex md:flex-col w-full md:w-20 items-center justify-center border-2 border-green-500 px-4 py-2 text-mobile-body-1-regular lg:text-body-1-regular hover:bg-green-100 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-300"
+              className='flex md:flex-col w-full md:w-20 items-center justify-center border-2 border-green-500 px-4 py-2 text-mobile-body-1-regular lg:text-body-1-regular hover:bg-green-100 disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-300'
             >
               {!authIsLoading && (
                 <>
-                  <span role="img" aria-label="save">
+                  <span role='img' aria-label='save'>
                     💾
                   </span>
                   Save
                 </>
               )}
               {authIsLoading && (
-                <RotatingLoader className="size-6 border-[3px] border-green-500" />
+                <RotatingLoader className='size-6 border-[3px] border-green-500' />
               )}
             </Button>
             <Button
               onClick={handleCancelClick}
-              className="flex md:flex-col w-full md:w-20 justify-center border-2 border-red-500 px-4 py-2 text-mobile-body-1-regular lg:text-body-1-regular hover:bg-red-100"
+              className='flex md:flex-col w-full md:w-20 justify-center border-2 border-red-500 px-4 py-2 text-mobile-body-1-regular lg:text-body-1-regular hover:bg-red-100'
             >
-              <span role="img" aria-label="cancel">
+              <span role='img' aria-label='cancel'>
                 ❌
               </span>
               Cancel
@@ -204,9 +204,9 @@ const PersonalInformation = ({ data }: PersonalInformationProps) => {
         ) : (
           <Button
             onClick={handleEditClick}
-            className="w-full md:w-32 py-3 border-2 border-[#E5E5E5] text-mobile-body-1-regular lg:text-body-1-regular hover:bg-gray-100"
+            className='w-full md:w-32 py-3 border-2 border-[#E5E5E5] text-mobile-body-1-regular lg:text-body-1-regular hover:bg-gray-100'
           >
-            <span role="img" aria-label="pencil">
+            <span role='img' aria-label='pencil'>
               ✏️
             </span>
             Edit Profile
